66000b65c1eb44f065380da57847a5f02c525070
Haoming Xia <mrmist3r311@gmail.com>
2021-04-18 16:31:04 -0700

    .

--- app.js
+++ app.js
@@ -2,10 +2,15 @@ const express = require("express");
 const app = express();
 const port = 8000;
 
+// import routes
+const index = require("routes/index.js")
+
 app.get("/", (req, res) => {
-  res.send("This does nothing");
+  res.send("API works");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`);
+  console.log(`API listening on port ${port}!`);
 });
+
+
